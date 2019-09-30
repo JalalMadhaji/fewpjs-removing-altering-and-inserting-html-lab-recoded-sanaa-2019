08ff5a9fd8ddee8f567c8834b6984e603511048a
Jalal Al-Madhaji <jalal.almadhaji90@gmail.com>
2019-09-30 00:16:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code here!
 document.querySelector('main#main').remove();
-let newHeader = document.createElement('h1#victory');
+let newHeader = document.createElement('h1');
 newHeader.innerHTML = "JALAL is the champion";
 document.body.appendChild(newHeader);
